4b92359a604a95ffef1af8c2bf0f5f774f339fe2
sbmvirdi <shubhamvirdic3@gmail.com>
2020-03-23 16:29:26 +0530

    added UI components to app

--- app/src/main/java/com/example/internalstorage/MainActivity.java
+++ app/src/main/java/com/example/internalstorage/MainActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -18,14 +22,35 @@ public class MainActivity extends AppCompatActivity {
     private static final String FILENAME = "shubamvirdi";
     private StringBuilder sb;
     private BufferedReader br;
+    private EditText value;
+    private Button readbtn,writebtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        value = findViewById(R.id.value);
+        readbtn = findViewById(R.id.readbtn);
+        writebtn = findViewById(R.id.writebtn);
+
+        readbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                readData();
+            }
+        });
+
+        writebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writeData();
+            }
+        });
+
+
 //         writeData();
-        readData();
+//         readData();
 
 
 
@@ -43,7 +68,8 @@ public class MainActivity extends AppCompatActivity {
                 line = br.readLine();
             }
             String content = sb.toString();
-            Log.e("VALUE",content);
+            this.value.setText(content);
+            Toast.makeText(this, "Data Read Successfully", Toast.LENGTH_SHORT).show();
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -52,13 +78,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void writeData(){
-        String value  = "sample data to write";
+        String value  = this.value.getText().toString();
         FileOutputStream fos = null;
         try {
             fos = this.openFileOutput(FILENAME,MODE_PRIVATE);
             OutputStreamWriter osw = new OutputStreamWriter(fos);
             osw.write(value);
             osw.close();
+            Toast.makeText(this, "Data Written Successfully", Toast.LENGTH_SHORT).show();
+            this.value.setText("");
         } catch (Exception e) {
             e.printStackTrace();
         }
